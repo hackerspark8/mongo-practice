a610ac35791c512a0108ccc79a097abcb92b267a
hackerspark <hackerspark@gmail.com>
2018-10-20 14:25:48 +0530

    Added GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const { ObjectID } = require("mongodb");
 
 const { mongoose } = require("./db/mongoose");
 const { Todo } = require("./models/todo");
@@ -24,6 +25,21 @@ app.get("/todos", async (req, res) => {
     res.send({ todos });
   } catch (e) {
     res.status(400).send(e);
+  }
+});
+app.get("/todos/:id", async (req, res) => {
+  const id = req.params.id;
+  if (!ObjectID.isValid(id)) {
+    return res.status(400).send();
+  }
+  try {
+    const todo = await Todo.findById(id);
+    if (!todo) {
+      return res.status(404).send();
+    }
+    res.send({ todo });
+  } catch (e) {
+    return res.status(400).send();
   }
 });
 app.listen(3000, () => console.log("Started on port 3000"));
