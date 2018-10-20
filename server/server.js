3b882be56e129d4315b08b5c58a990314c45fdce
hackerspark <hackerspark@gmail.com>
2018-10-20 11:02:22 +0530

    Added GET /todos

--- server/server.js
+++ server/server.js
@@ -18,6 +18,14 @@ app.post("/todos", async (req, res) => {
     res.status(400).send(e);
   }
 });
+app.get("/todos", async (req, res) => {
+  try {
+    const todos = await Todo.find();
+    res.send({ todos });
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
 app.listen(3000, () => console.log("Started on port 3000"));
 
 module.exports = { app };
