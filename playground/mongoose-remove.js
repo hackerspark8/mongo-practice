ea9fcedcbcd8c1b4f397fb190720f1779692e160
hackerspark <hackerspark@gmail.com>
2018-10-21 13:04:28 +0530

    Playing with delete

---
+++ playground/mongoose-remove.js
@@ -0,0 +1,6 @@
+const { ObjectID } = require("mongodb");
+
+const { mongoose } = require("../server/db/mongoose");
+const { Todo } = require("../server/models/todo");
+
+Todo.remove({}).then(result => console.log(result));
